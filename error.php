6ade0e7868c77d563723accd17a8822809db2bed
root <workspaceyasen@gmail.com>
2022-01-04 22:49:08 +0100

    htaccess

--- error.php
+++ error.php
@@ -1,1 +1,12 @@
-hola
+<?php
+
+require_once './views/layout/header.php';
+
+require_once './views/layout/sidebar.php';
+
+?>
+<h1>ERROR 404</h1>
+<?php
+require_once './views/layout/footer.php';
+
+?>
--- index.php
+++ index.php
@@ -3,31 +3,42 @@
 require_once './controllers/CategoriaController.php';
 require_once './controllers/PedidoController.php';
 require_once './controllers/ProductoController.php';
-require_once './controllers/UsuarioController.class.php';
+require_once './controllers/UsuarioController.php';
+require_once './controllers/ErrorController.php';
 require_once './views/layout/header.php';
 require_once './views/layout/sidebar.php';
+require_once './config/parameters.php';
 
+
+function show_error()
+{
+    $error = new errorController();
+    $error->index();
+}
 
 if (isset($_GET['controller'])) {
-    $nombre_controlador = $_GET['controller'].'Controller';
-    if (class_exists($nombre_controlador)) {
-        $controlador = new $nombre_controlador();
-        if (isset($_GET['action']) && method_exists($controlador, $_GET['action'])) {
-            
-            $action = $_GET['action'];
-            $controlador->$action();
-        } else {
-            echo "La pagina que buscas no existe1";
-        }
-    } else {
-        echo "La pagina que buscas no existe2";
-    } 
+    $nombre_controlador = $_GET['controller'] . 'Controller';
+} elseif(!isset($_GET['controller']) && !isset($_GET['action'])) {
+    $nombre_controlador = controller_default;
+}else {
+    show_error();
+    exit();
+}
+
+if (class_exists($nombre_controlador)) {
+    $controlador = new $nombre_controlador();
+    if (isset($_GET['action']) && method_exists($controlador, $_GET['action'])) {
+        $action = $_GET['action'];
+        $controlador->$action();
+    } elseif(!isset($_GET['controller']) && !isset($_GET['action'])) {
+        $action_default = action_default;
+        $controlador->index();     
+    }else {
+        show_error();
+    }
 } else {
-    echo "La pagina que buscas no existe3";
+    show_error();
 }
 
 
 require_once './views/layout/footer.php';
-
-
-
